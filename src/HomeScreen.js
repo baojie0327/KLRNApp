055bfbb4fe674b1eb3dca99cd3557e533d227b88
Jackson <bj0327_work@163.com>
2020-09-02 17:14:59 +0800

    开始做bottom tab

---
+++ src/HomeScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {StyleSheet, View, Text, Image} from 'react-native';
+import App from '../App';
+
+
+export default class HomeScreen extends Comment{
+
+    render(){
+        return (
+            <View>
+                <Text>首页</Text>
+            </View>
+
+        );
+    }
+
+};
+
